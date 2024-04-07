fe4bd6378e42f27b38fbf85f81db264be096254b
zhengzonggang <zhengzonggang@qudian.com>
2024-04-07 11:21:11 +0800

    feat: basic structure

--- App.tsx
+++ App.tsx
@@ -5,114 +5,82 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { ReactElement } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  FlatList,
+  TouchableWithoutFeedback,
   View,
+  Text
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import FadeInView from './animated/FadeInView';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+        <Text style={styles.title}>Aniamted</Text>
+
+        <FlatList
+        style={styles.list}
+        data={[
+            {
+                type : 'Fade In',
+                page : <FadeInView/>
+            }
+        ]}
+        renderItem={({item}) => {
+            
+            return (
+                <TouchableWithoutFeedback>
+                    <View style={styles.row}>
+                        <Text style={styles.text}>{item.type}</Text>
+                        <View style={styles.line}></View>
+                    </View>                    
+                </TouchableWithoutFeedback>
+            )
+        }}
+        >
+
+        </FlatList>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    list: {
+        // backgroundColor: 'green',
+    },
+    title: {
+        color: 'green',
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center'        
+    },
+
+    row: {
+        height: 70,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green',
+        margin: 10,
+        borderRadius: 20
+    },
+    text: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    line: {
+        backgroundColor: 'white',
+        height: 1,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0
+    }
 });
 
 export default App;
---
+++ animated/FadeInView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FadeInView: React.FC = () => {
+    return <></>
+}
+
+export default FadeInView
